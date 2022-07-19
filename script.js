d07e1d8d225fcb61747a20a527a775eddb447c83
milenasandra <zsandra745@gmail.com>
2022-07-18 21:14:00 -0500

    Esta es la entrega de la actividad 1 de JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = 'sandra'
+let lastName = 'zuñiga'
+let dateOfBirth = '1998'
+let city = 'Medellín'
+
+console.log(`hola, mi nombre es' ${name} ${lastName}, vivo en ${city}, y tengo ${2022-dateOfBirth} años`)
